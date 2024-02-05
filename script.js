2aa3b51ae63941a33f77fa1b63f3e9e430f20891
Yordani <yalvarezb93@gmail.com>
2024-02-05 19:52:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let hour = document.getElementById('hour');
+let min = document.getElementById('min');
+let second = document.getElementById('second');
+
+
+setInterval(() => {
+    const time = new Date();
+
+    hour.innerHTML = time.getHours() < 10 ? '0' + time.getSeconds() : time.getHours();;
+    min.innerHTML = time.getMinutes() < 10 ? '0' + time.getSeconds() : time.getMinutes();;
+    second.innerHTML = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds();
+}, 1000);
